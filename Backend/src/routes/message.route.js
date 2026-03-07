0010c50ac7e107ac86efec998998ae7bc64f7e3b
Yadhu <yadhuy28@gmail.com>
2026-03-08 02:00:15 +0530

    fix: sending image complete

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import {getMessages,getUsersSidbar,sendMessage} from '../controllers/message.controll.js'
 import protectRoute from '../middlewares/auth.middleware.js'
+import { uploadImage } from '../middlewares/upload.js'
 const router = Router()
 
 
 router.get("/users",protectRoute,getUsersSidbar)
 router.get("/:id",protectRoute,getMessages)
-router.post('/send/:id',protectRoute,sendMessage)
+router.post('/send/:id', protectRoute, uploadImage, sendMessage)
 
 
 export default router
